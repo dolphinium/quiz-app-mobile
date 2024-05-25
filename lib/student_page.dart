021dd62bd59d09502f0acbe56cfdac8b9af5db61
dolphinium <dolphofficial@gmail.com>
2024-05-25 17:50:28 +0300

    v2

--- lib/student_page.dart
+++ lib/student_page.dart
@@ -18,7 +18,7 @@ class _StudentPageState extends State<StudentPage> {
 
   Future<void> _fetchQuizzes() async {
     try {
-      final response = await http.get(Uri.parse('http://localhost:8080/api/quizzes/getAll'));
+      final response = await http.get(Uri.parse('http://localhost:8080/api/quizzes'));
       final quizzes = json.decode(response.body);
       setState(() {
         _quizzes = quizzes;
@@ -26,6 +26,15 @@ class _StudentPageState extends State<StudentPage> {
     } catch (error) {
       // Handle error
     }
+  }
+
+  void _navigateToQuiz(BuildContext context, dynamic quiz) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => QuizPage(quiz: quiz),
+      ),
+    );
   }
 
   @override
@@ -38,10 +47,113 @@ class _StudentPageState extends State<StudentPage> {
         itemCount: _quizzes.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(_quizzes[index]['question']),
-            subtitle: Text(_quizzes[index]['answer']),
+            title: Text(_quizzes[index]['title']),
+            onTap: () => _navigateToQuiz(context, _quizzes[index]),
           );
         },
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  final dynamic quiz;
+
+  QuizPage({required this.quiz});
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  Map<int, int> _answers = {};
+  int _correctCount = 0;
+  int _incorrectCount = 0;
+  int _blankCount = 0;
+  bool _submitted = false;
+
+  void _submitQuiz() {
+    int correct = 0;
+    int incorrect = 0;
+    int blank = 0;
+
+    for (int i = 0; i < widget.quiz['questions'].length; i++) {
+      if (_answers.containsKey(i)) {
+        bool isCorrect = widget.quiz['questions'][i]['answers'][_answers[i]]['isCorrect'];
+        if (isCorrect) {
+          correct++;
+        } else {
+          incorrect++;
+        }
+      } else {
+        blank++;
+      }
+    }
+
+    setState(() {
+      _correctCount = correct;
+      _incorrectCount = incorrect;
+      _blankCount = blank;
+      _submitted = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.quiz['title']),
+      ),
+      body: _submitted
+          ? Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Correct Answers: $_correctCount'),
+            Text('Incorrect Answers: $_incorrectCount'),
+            Text('Blank Answers: $_blankCount'),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Back to Quizzes'),
+            ),
+          ],
+        ),
+      )
+          : ListView.builder(
+        itemCount: widget.quiz['questions'].length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.quiz['questions'][index]['text']),
+                  ...List.generate(
+                    widget.quiz['questions'][index]['answers'].length,
+                        (answerIndex) => RadioListTile<int>(
+                      title: Text(widget.quiz['questions'][index]['answers'][answerIndex]['text']),
+                      value: answerIndex,
+                      groupValue: _answers[index],
+                      onChanged: (int? value) {
+                        setState(() {
+                          _answers[index] = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: _submitted
+          ? null
+          : FloatingActionButton(
+        onPressed: _submitQuiz,
+        child: Icon(Icons.check),
       ),
     );
   }
--- lib/teacher_page.dart
+++ lib/teacher_page.dart
@@ -9,19 +9,17 @@ class TeacherPage extends StatefulWidget {
 
 class _TeacherPageState extends State<TeacherPage> {
   TextEditingController _quizTitleController = TextEditingController();
-  TextEditingController _questionController = TextEditingController();
-  TextEditingController _answerController = TextEditingController();
+  List<Question> _questions = [];
 
   Future<void> _createQuiz() async {
     final quiz = {
       'title': _quizTitleController.text,
-      'question': _questionController.text,
-      'answer': _answerController.text,
+      'questions': _questions.map((q) => q.toJson()).toList(),
     };
 
     try {
       await http.post(
-        Uri.parse('http://localhost:8080/api/quizzes/add'),
+        Uri.parse('http://localhost:8080/api/quizzes'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode(quiz),
       );
@@ -29,6 +27,12 @@ class _TeacherPageState extends State<TeacherPage> {
     } catch (error) {
       // Handle error
     }
+  }
+
+  void _addQuestion() {
+    setState(() {
+      _questions.add(Question());
+    });
   }
 
   @override
@@ -45,13 +49,25 @@ class _TeacherPageState extends State<TeacherPage> {
               controller: _quizTitleController,
               decoration: InputDecoration(labelText: 'Quiz Title'),
             ),
-            TextField(
-              controller: _questionController,
-              decoration: InputDecoration(labelText: 'Enter Question'),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _questions.length,
+                itemBuilder: (context, index) {
+                  return QuestionWidget(
+                    question: _questions[index],
+                    onRemove: () {
+                      setState(() {
+                        _questions.removeAt(index);
+                      });
+                    },
+                  );
+                },
+              ),
             ),
-            TextField(
-              controller: _answerController,
-              decoration: InputDecoration(labelText: 'Enter Answer'),
+            ElevatedButton(
+              onPressed: _addQuestion,
+              child: Text('Add Question'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
@@ -61,6 +77,110 @@ class _TeacherPageState extends State<TeacherPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class Question {
+  TextEditingController textController = TextEditingController();
+  List<Answer> answers = [Answer()];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': textController.text,
+      'answers': answers.map((a) => a.toJson()).toList(),
+    };
+  }
+}
+
+class Answer {
+  TextEditingController textController = TextEditingController();
+  bool isCorrect = false;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': textController.text,
+      'isCorrect': isCorrect,
+    };
+  }
+}
+
+class QuestionWidget extends StatelessWidget {
+  final Question question;
+  final VoidCallback onRemove;
+
+  QuestionWidget({required this.question, required this.onRemove});
+
+  void _addAnswer() {
+    question.answers.add(Answer());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: question.textController,
+              decoration: InputDecoration(labelText: 'Question'),
+            ),
+            SizedBox(height: 10),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: question.answers.length,
+              itemBuilder: (context, index) {
+                return AnswerWidget(answer: question.answers[index]);
+              },
+            ),
+            ElevatedButton(
+              onPressed: _addAnswer,
+              child: Text('Add Answer'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: onRemove,
+              child: Text('Remove Question'),
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnswerWidget extends StatefulWidget {
+  final Answer answer;
+
+  AnswerWidget({required this.answer});
+
+  @override
+  _AnswerWidgetState createState() => _AnswerWidgetState();
+}
+
+class _AnswerWidgetState extends State<AnswerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: widget.answer.textController,
+            decoration: InputDecoration(labelText: 'Answer'),
+          ),
+        ),
+        Checkbox(
+          value: widget.answer.isCorrect,
+          onChanged: (bool? value) {
+            setState(() {
+              widget.answer.isCorrect = value ?? false;
+            });
+          },
+        ),
+      ],
     );
   }
 }
